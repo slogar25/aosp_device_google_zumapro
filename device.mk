fb89df9165e005f61551d96c1a6469cf932b1eea
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2024-08-22 12:36:30 +0300

    add missing FCM definitions for some proprietary HALs

--- device.mk
+++ device.mk
@@ -1291,6 +1291,9 @@ include hardware/google/pixel/wifi_ext/device.mk
 # Battery Stats Viewer
 PRODUCT_PACKAGES_DEBUG += BatteryStatsViewer
 
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/android.hardware.gnss.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/proprietary/vendor.google.aam.xml
+
 # Keymint configuration
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
