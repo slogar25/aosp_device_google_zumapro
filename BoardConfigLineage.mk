ae03a93628d5e868c190d80174ff482eb99e82db
Michael Bestas <mikeioannina@gmail.com>
2024-09-18 02:15:06 +0300

    Add android.hardware.gnss AIDL to compatibility matrix
    Change-Id: I131aafa886ec5a1eef8a104fead2821a7dac56cd

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -8,6 +8,8 @@
 DEVICE_MANIFEST_FILE += \
     device/google/zumapro/android.hardware.security.rkp-service.citadel.xml \
     device/google/zumapro/manifest_radio_ds.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
+    device/google/zumapro/location/device_framework_matrix_product.xml
 
 # Partitions
 AB_OTA_PARTITIONS += \
