3654b371aec57e1d5d9d54788232fbe0ccca2dd6
slogar25 <michaelslogar@gmail.com>
2025-05-22 10:26:04 +0000

    add device-omni.mk
    Change-Id: Ib67a877099f3e14d7ba6192b87ef74d726475f24

---
+++ device-omni.mk
@@ -0,0 +1,23 @@
+DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-omni
+
+# Camera
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
+    ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.camerax.extensions.enabled=true
+
+# EUICC
+PRODUCT_PACKAGES += \
+    EuiccSupportPixelOverlay
+
+# custom A/B postinstall
+PRODUCT_PACKAGES += \
+    omnipreopt_script
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/omnipreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
